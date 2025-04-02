288224c82e47e7b58726c7e135e86017df2f1931
Volodymyr Khalus <vntu0311051@gmail.com>
2025-04-02 21:04:08 +0300

    module3_l2

--- src/components/BookList/BookItem.jsx
+++
@@ -1,5 +0,0 @@
-const BookItem = ({ book }) => {
-  return <li>{book.name}</li>;
-};
-
-export default BookItem;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./components/App/App";
+import FeedbackForm from "./components/FeedbackForm/FeedbackForm";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <FeedbackForm />
   </StrictMode>
 );
